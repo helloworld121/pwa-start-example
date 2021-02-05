2baddc9cd9c400cbc1e48d04e90c56a2ce22ceda
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-02-05 15:02:56 +0100

    service worker: lifecycle method "activate"

--- public/sw.js
+++ public/sw.js
@@ -13,6 +13,14 @@
 // 1) on every html-page
 // 2) app.js => if it is registered on every html-page => than we only need to register it once
 
+// ACTIVATE:
+// to activate a new ServiceWorker or if the ServiceWorker-code was CHANGED
+// => it is necessary to CLOSE the browser tab
+// the behaviour is based on the fact, that replacing a current ServiceWorker might break the page
+//    because the page might interact with the current ServiceWorker
+// => it is also possible to use DevTools in the Browser to reload the ServiceWorker
+//    configuration to auto-reload
+
 
 // IMPLEMENTATION:
 // "self" gives access to the ServiceWorker itself
@@ -21,6 +29,11 @@ self.addEventListener('install', function(event){
     console.log('[Service Worker] Installing ServiceWorker...', event);
 });
 
+// ATTENTION:
+// ACTIVATING a ServiceWorker won't work on a running page
+// => because the current page might interact with an old version of the service worker
+//    and activating the new one might break the application
+// => it is necessary to close the "tab" or "browser window" to activate the new ServiceWorker
 self.addEventListener('activate', function(event){
     console.log('[Service Worker] Activating ServiceWorker...', event);
     return self.clients.claim(); // this line is necessary right now
